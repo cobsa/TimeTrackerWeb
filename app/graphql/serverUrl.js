dddb9011e59ebec756ad53d8cba78ee417688b21
Jaakko Koponen <cobsa89@gmail.com>
2018-05-31 18:43:14 +0300

    Added production URI

--- app/graphql/serverUrl.js
+++ app/graphql/serverUrl.js
@@ -1,4 +1,6 @@
 const uri =
-  process.env.NODE_ENV === 'production' ? 'production_graphql' : 'http://localhost:3000/graphql'
+  process.env.NODE_ENV === 'production'
+    ? 'ec2-35-180-119-122.eu-west-3.compute.amazonaws.com:3000/graphql'
+    : 'http://localhost:3000/graphql'
 
 export default uri
